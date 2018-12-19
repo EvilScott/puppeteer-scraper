3474e455128da41ed22d0fc6f59b0f6ed1f2b319
Robert Scott Reis <reis.robert.s@gmail.com>
2018-12-19 10:24:15 -0800

    handle missing url, dont load uneeded resources, and add a timeout of 3s

--- index.js
+++ index.js
@@ -6,18 +6,43 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/scrape', async (req, res) => {
+  // collect params from request
+  const timeout = parseInt(req.body.timeout) || 3 * 1000;
   const url = req.body.url;
-  // TODO handle no url
+
+  // bail out if no url
+  if (!url) {
+    return res.sendStatus(400);
+  }
+
   try {
+    // create a new page from the shared browser instance
     const page = await browser.newPage();
-    await page.goto(url, { waitUntil: 'load' });
+
+    // block images, stylesheets, and fonts
+    await page.setRequestInterception(true);
+    page.on('request', (request) => {
+      if ([ 'image', 'stylesheet', 'font' ].indexOf(request.resourceType()) !== -1) {
+        request.abort();
+      } else {
+        request.continue();
+      }
+    });
+
+    // load the page
+    await page.goto(url, { timeout, waitUntil: 'load' });
+
+    // scrape the content and pass it along
     const content =  await page.content();
     res.send(content);
+
+  // pass along any error info
   } catch (err) {
-    res.status(500).json({ err });
+    res.status(500).send(err.toString());
   }
 });
 
+// share a browser instance between requests
 let browser;
 (async () => {
   const args = [ '--no-sandbox', '--disable-setuid-sandbox' ];
